bf6a16e3d3448dc305a4775f0fb4224ff7c8c6c5
Kenneth Johansen Misund <77608468+TimeTo-Slam@users.noreply.github.com>
2022-11-18 15:09:13 +0100

    Character development
    Have setup the animation for the character and also fixed the scripts.

--- Flaming-Tower/Assets/Scripts/PlayerController.cs
+++ Flaming-Tower/Assets/Scripts/PlayerController.cs
@@ -1,26 +1,47 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class PlayerController : MonoBehaviour
 {
 
-	public float MovementSpeed = 1;
-	public float JumpForce = 10;
+	public float MovementSpeed = 10f;
+	public float JumpForce = 20f;  
+  float movement = 0f;
+  
+  bool isFacingRight = true;
 
+  public Animator animator;
 	private Rigidbody2D rb;
 
     // Start is called before the first frame update
     void Start()
     {
-		rb = GetComponent<Rigidbody2D>();
-        
+		rb = GetComponent<Rigidbody2D>();   
+    }
+
+    void flipPlayer() {
+      isFacingRight = !isFacingRight;
+      transform.Rotate(0f, 180f, 0f);
     }
 
     // Update is called once per frame
-    void Update()
+    void Update() 
     {
-		var movement = Input.GetAxis("Horizontal");
+
+		movement = Input.GetAxis("Horizontal");
+
+    if (movement < 0 && isFacingRight) {
+      flipPlayer();
+    }
+    else if (movement > 0 && !isFacingRight)
+    {
+      flipPlayer();
+    }
+
+
+    animator.SetFloat("Speed", Mathf.Abs(movement));
 		transform.position += new Vector3(movement, 0, 0) * Time.deltaTime * MovementSpeed;
 
         if(Input.GetButtonDown("Jump") && Mathf.Abs(rb.velocity.y) < 0.001f) 
