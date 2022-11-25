e6898ee9a03d594151a7e112555cb6b795ec1968
Ivar <ivabr@stud.ntnu.no>
2022-11-26 00:08:43 +0100

    Created a way for the lava to increase the higher the score value.

--- Flaming-Tower/Assets/Scripts/Lava/MovingLava.cs
+++ Flaming-Tower/Assets/Scripts/Lava/MovingLava.cs
@@ -12,6 +12,13 @@ public class MovingLava : MonoBehaviour
     [Tooltip("The speed of the movement of the lava.")]
     public float lavaSpeed = 5f;
     
+    [Tooltip("The amount the lava speed will increase by")]
+    public float lavaSpeedIncrease = 0.2f;
+    
+    [Tooltip("The value the score can be divide by. " +
+             "Example if set to 5 then speed will increase when score is 5,10,15...")]
+    public int scoreDivider = 10;
+
     private int _score;
     private bool _stopRising;
 
@@ -33,7 +40,18 @@ public class MovingLava : MonoBehaviour
         if (_stopRising) return;
         // Updating the position (Making the lava rise)
         transform.position += new Vector3(0, lavaSpeed / 1000, 0);
+        SpeedUpLava();
 
+    }
+
+    private void SpeedUpLava()
+    {
+        _score = GameManager.gameManager.GetScore();
+        if (_score % scoreDivider == 0)
+        {
+            lavaSpeed += lavaSpeedIncrease;
+        }
+        
     }
 
     public void LavaRise(bool lavaRise)
