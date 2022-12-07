c7b1987010b094cc3146920b341848f485ee6759
Joakim <joakimroren@gmail.com>
2022-12-07 23:50:44 +0100

    updated documentation and made some minor changes to some scripts.
    updated documentation and made some minor changes to some scripts.

--- Flaming-Tower/Assets/Scripts/Intro/AudioAdjust.cs
+++ Flaming-Tower/Assets/Scripts/Intro/AudioAdjust.cs
@@ -1,17 +1,27 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
+/**
+ * This class is responsible for adjusting the audio.
+ */
 public class AudioAdjust : MonoBehaviour
 {
+    [Header("Audio Settings")]
+    [Tooltip("The audio source to be adjusted")]
     [SerializeField] private AudioSource audioSource;
 
+    /// <summary>
+    /// Reads the music volume on awake
+    /// </summary>
     private void Awake()
     {
         ReadMusicVolume();
     }
 
+    /// <summary>
+    /// Checks if there is a saved music volume.
+    /// If not, one will be created.
+    /// If exist sets the adui slider position and volume based on the volume read.l
+    /// </summary>
     private void ReadMusicVolume()
     {
         if (!PlayerPrefs.HasKey("musicVolume"))
@@ -22,6 +32,9 @@ public class AudioAdjust : MonoBehaviour
         audioSource.volume = PlayerPrefs.GetFloat("musicVolume");
     }
     
+    /**
+     * Sets the default music volume.
+     */
     private void SetDefaultMusicVolume()
     {
         PlayerPrefs.SetFloat("musicVolume", 1);
--- Flaming-Tower/Assets/Scripts/Intro/UIFadeIn.cs
+++ Flaming-Tower/Assets/Scripts/Intro/UIFadeIn.cs
@@ -2,42 +2,51 @@ using UnityEngine;
 
 public class UIFadeIn : MonoBehaviour
 {
-    [SerializeField] private CanvasGroup canvasGroup;
-    [SerializeField] private float maxFade = 1f;
-    [SerializeField] private float minFade = 0f;
-    [SerializeField] private bool fadeIn;
+    [Header("Fade Settings")]
+    [Tooltip("The canvas to be faded. Must have 'Canvas Group' as a component to the canvas to be faded.")]
+    [SerializeField]
+    private CanvasGroup canvasGroup;
 
-    public float timeToFade;
-    public float timeToChangeScene;
+    [Tooltip("Maximum fading at the highest. 1f equals 100% visibility.")] [SerializeField]
+    private float maxFade = 1f;
 
+    [Tooltip("Minimum fading at the highest. 0f equals 0% visibility.")] [SerializeField]
+    private float minFade = 0f;
+
+    [Tooltip("Canvas will only be faded in if this is toggled.")] [SerializeField]
+    private bool fadeIn;
+
+    [Tooltip("The force to be applied to the fading")]
+    public float fadeForce = 0.2f;
+
+    /// <summary>
+    /// This method is called for every frame, as long as MonoBehaviour is being used.
+    /// </summary>
     private void Update()
     {
-        if (fadeIn)
-        {
-            FadeIn();
-        }
+        FadeIn();
     }
 
+    /// <summary>
+    /// Sets the alhpa to minFade value. This defines whats the highest visibility amount.
+    /// Also starts fading out.
+    /// </summary>
     void OnEnable()
     {
-        if (fadeIn)
-        {
-            canvasGroup.alpha = minFade;
-            FadeIn();
-        }
+        canvasGroup.alpha = minFade;
     }
 
+    /// <summary>
+    /// Fades in the canvas until it reaches the maximum fade value specified by maxFade.
+    /// </summary>
     public void FadeIn()
     {
-        if (fadeIn)
+        if (canvasGroup.alpha < maxFade)
         {
-            if (canvasGroup.alpha < 1)
+            canvasGroup.alpha += fadeForce * Time.deltaTime;
+            if (canvasGroup.alpha >= maxFade)
             {
-                canvasGroup.alpha += timeToFade * Time.deltaTime;
-                if (canvasGroup.alpha >= maxFade)
-                {
-                    fadeIn = false;
-                }
+                fadeIn = false;
             }
         }
     }
